0f4eb79c1d07fd5743bb821d41b141d28a3b2faf
Bo Liu <bo.liu@modicagroup.com>
2022-05-12 17:02:02 +1200

    finish off module 2 take 2

--- alog.go
+++ alog.go
@@ -39,7 +39,8 @@ func New(w io.Writer) *Alog {
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
 func (al Alog) Start() {
-	for msg := range al.msgCh {
+	for {
+		msg := <-al.msgCh
 		go func(m string) {
 			al.write(m, nil)
 		}(msg)
